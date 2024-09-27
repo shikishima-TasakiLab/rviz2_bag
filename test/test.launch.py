d4c25c37c15be703d27fe9d620ecdc73e7a2d986
shikishima-TasakiLab <160442065@ccalumni.meijo-u.ac.jp>
2024-09-27 05:13:34 +0000

    Add test environment.

---
+++ test/test.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+import os.path
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            namespace='',
+            executable='static_transform_publisher',
+            arguments=[
+                '--frame-id', 'base_link',
+                '--child-frame-id', 'velodyne_top',
+            ],
+        ),
+        Node(
+            package='rviz2',
+            namespace='',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d' + os.path.join(get_package_share_directory('rviz2_bag'), 'test', 'test.rviz')]
+        )
+    ])
